3071a790b4ead18c1d05a948295887f236e5f40a
alfeyo <alfeyokatebe@gmail.com>
2021-06-17 08:40:44 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import './css/main.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 //import NavbarM from './components/navbar';
 
 function App() {
   return (
 
 
-    <div className="slideshow">
-      <div className="slideshow-item">
-        <img scr={""} alt="header_image" />
-      </div>
+    <div>
+
     </div>
   );
 }
